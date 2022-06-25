45145e834bbf0b2f60286318e8c6a79167d6b8fe
Taofikat <usmantaofeeqah2011@gmail.com>
2022-06-25 10:53:21 +0100

    second commit

--- myself.js
+++ myself.js
@@ -1,7 +1,13 @@
-let NAME = 'Taofikat Okeoluwa Usman';
-let HEIGHT = '1.60m';
-let COUNTRY = 'Nigeria';
+let name = 'Taofikat Okeoluwa Usman';
+let height = '1.60m';
+let country = 'Nigeria';
 
-console.log(NAME);
-console.log(HEIGHT);
-console.log(COUNTRY);
+// console.log(name);
+// console.log(height);
+// console.log(country);
+
+// document.write('Name:' ,' ' + name ,'<br/>'); 
+// document.write('Height:' , ' ' + height ,'<br>');
+// document.write('Country:' ,' ' + country);
+
+alert('Name: Taofikat Okeoluwa Usman \nHeight: 1.60m \nCountry: Nigeria');
